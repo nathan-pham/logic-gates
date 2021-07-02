b02f0addce86c9002a6ee9dcf234579af3526044
Nathan Pham <43102813+nathan-pham@users.noreply.github.com>
2021-07-02 03:37:34 +0000

    compiled adder

--- index.js
+++ index.js
@@ -22,3 +22,54 @@ const truth_table = [
 
 // test custom circuit
 test(truth_table, custom_xor)
+
+// parseInt(binary, 2)
+
+// more complicated circuits: half-adder
+// returns [sum, cout]
+const half_adder = (a, b) => ([
+  atoms.xor(a, b),
+  atoms.and(a, b)
+])
+
+// full-adder, composed of 2 half-adders
+// returns [sum, cout]
+const full_adder = (a, b, cin=0) => {
+  let [s1, c1] = half_adder(a, b)
+  let [s2, c2] = half_adder(s1, cin)
+
+  return [
+    s2,
+    atoms.or(c1, c2)
+  ]
+}
+
+const to_decimal = (results) => parseInt(results.reverse().join(''), 2)
+const to_binary = (decimal) => Number(decimal).toString(2)
+const format = (num) => to_binary(num).split('').map(n => parseInt(n))
+
+
+console.log("\nI can add 1 + 1! yay!", to_decimal(full_adder(1, 1)))
+
+// composed_adders(decimal_value, decimal_value)
+// returns [sum, cout]
+const composed_adders = (n1, n2) => {
+  n1 = format(n1).reverse()
+  n2 = format(n2).reverse()
+  
+  const adders = new Array(Math.max(n1.length, n2.length)).fill(full_adder)
+  let total_sum = []
+  let cache_cout = 0
+
+  for(let i = 0; i < adders.length; i++) {
+    const adder = adders[i]
+
+    let [sum, cout] = adder(n1[i] || 0, n2[i] || 0, cache_cout)
+    total_sum.push(sum)
+    cache_cout = cout
+  }
+
+  return [total_sum, cache_cout]
+}
+
+console.log("I can add 2 + 10 too!", to_decimal(composed_adders(2, 10)[0]))
